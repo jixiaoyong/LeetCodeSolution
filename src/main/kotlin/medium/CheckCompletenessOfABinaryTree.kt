064c52649e4b370d48b6a79870f803de6be9a4a0
jixiaoyong <jixiaoyong1995@gmail.com>
2022-01-07 11:32:55 +0800

    feat: 增加958. 二叉树的完全性检验另外两种解法

--- src/main/kotlin/medium/CheckCompletenessOfABinaryTree.kt
+++ src/main/kotlin/medium/CheckCompletenessOfABinaryTree.kt
@@ -75,10 +75,110 @@ object CheckCompletenessOfABinaryTree {
 
         return true
     }
+
+    /**
+     * BFS 广度优先搜索
+     * 思路：
+     * 先从左往右层遍历，如果遇到空节点，则记录下来
+     * 如果在遇到空节点之后，还有遇到非空节点，那么就不是完全二叉树
+     */
+    fun isCompleteTreeBfs(root: TreeNode?): Boolean {
+        if (root == null || (root.left == null && root.right == null)) {
+            return true
+        }
+
+        if (root.left == null) {
+            return false
+        }
+
+        var currentNodeList = mutableListOf(root)
+        var hasNullNode = false
+        while (currentNodeList.isNotEmpty()) {
+            val tempNodeList = mutableListOf<TreeNode>()
+            for (treeNode in currentNodeList) {
+                if (treeNode.left == null) {
+                    // 如果有空的标记一下
+                    hasNullNode = true
+                } else if (hasNullNode) {
+                    // 如果在非空值之前已经有空的了，则不是完全二叉树
+                    return false
+                } else {
+                    tempNodeList.add(treeNode.left!!)
+                }
+                if (treeNode.right == null) {
+                    hasNullNode = true
+                } else if (hasNullNode) {
+                    return false
+                } else {
+                    tempNodeList.add(treeNode.right!!)
+                }
+            }
+            currentNodeList = tempNodeList
+        }
+
+        return true
+    }
+
+
+    /**
+     * DFS 深度优先搜索
+     * 思路：
+     * 记录树所有节点数量
+     * 计算树是完全二叉树的时候应该有的节点数量
+     * 二者不相等则不是完全二叉树
+     */
+    fun isCompleteTreeDfs(root: TreeNode?): Boolean {
+        if (root == null || (root.left == null && root.right == null)) {
+            return true
+        }
+
+        if (root.left == null) {
+            return false
+        }
+
+        return dfs(root, 1, calculateTreeNodeNumber(root))
+    }
+
+    /**
+     * 判断是否完全二叉树
+     * dfs遍历node，判断期望的下标是否大于实际的节点总数
+     * （如果期望下标大于实际节点总数，则说明在其之前有null节点，则不是完全二叉树）
+     * 期望下标：按照二叉树的节点，对于下标为n的节点，其左节点下标为2n，其右节点下标为2n+1
+     * 实际下标：以root下标为1，按照从左往右，从上往下层次遍历编号的数目
+     * @param node 当前节点
+     * @param currentIndex 当前节点的期望下标
+     * @param totalNodeCount 整个二叉树所有节点总的数目
+     * @return 是否完全二叉树
+     */
+    private fun dfs(node: TreeNode?, currentIndex: Int, totalNodeCount: Int): Boolean {
+        if (node == null) {
+            return true
+        }
+
+        if (currentIndex > totalNodeCount) {
+            return false
+        }
+
+        return dfs(node.left, currentIndex * 2, totalNodeCount) && dfs(node.right, currentIndex * 2 + 1, totalNodeCount)
+    }
+
+    /**
+     * 计算treeNode实际节点数量
+     */
+    private fun calculateTreeNodeNumber(treeNode: TreeNode?): Int {
+        if (treeNode == null) {
+            return 0
+        }
+
+        val leftCount = calculateTreeNodeNumber(treeNode.left)
+        val rightCount = calculateTreeNodeNumber(treeNode.right)
+
+        return leftCount + rightCount + 1
+    }
 }
 
 fun main() {
     val nodeTree = Utils.createTreeFromString("[1,2,3,5,null,7,8]")
-    val isCompleteTree = CheckCompletenessOfABinaryTree.isCompleteTree(nodeTree)
-    print(isCompleteTree)
+    val isCompleteTree = CheckCompletenessOfABinaryTree.isCompleteTreeDfs(nodeTree)
+    println(isCompleteTree)
 }
